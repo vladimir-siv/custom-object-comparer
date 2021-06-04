d7e6921c89b46876d110f28bc1af3c9ade097ea2
vladimir-siv <vladimir.sivcev@gmail.com>
2021-06-04 19:49:05 +0200

    Project Setup

---
+++ src/custom-object-comparer/CustomObjectComparerTests/ComparisonTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CustomObjectComparerTests
+{
+	[TestClass]
+	public class ComparisonTests
+	{
+		[TestMethod]
+		public void Compare1()
+		{
+
+		}
+	}
+}
---
+++ src/custom-object-comparer/CustomObjectComparerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomObjectComparerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomObjectComparerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("442248d5-9fe5-4edb-a441-b096bf07854c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
